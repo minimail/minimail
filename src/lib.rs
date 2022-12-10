18bf6532a0558fc09f9c3b02368b093e6dda665a
James Gayfer <gayfer.james@gmail.com>
2022-12-10 12:02:38 -0800

    Add subscriber store
    Basic in memory store to get us going. Playing around with the new
    support for async traits natively.
    
    May want to think about moving the in memory implementation elsewhere as
    we grow, but seems okay for now.
    
    Also likely want proper error handling in the future via anyhow or
    similar.

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,4 @@
+#![feature(async_fn_in_trait)]
+
 pub mod model;
+pub mod store;
---
+++ src/store/mod.rs
@@ -0,0 +1,4 @@
+mod subscriber_store;
+
+pub use subscriber_store::InMemorySubscriberStore;
+pub use subscriber_store::SubscriberStore;
