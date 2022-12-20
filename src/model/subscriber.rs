db572cd4004e1905c7101eeadd6a9ced51385138
Derrick Plotsky <derrickplotsky@gmail.com>
2022-12-19 20:01:22 -0800

    Derive serialize/deserialize for subscriber models

--- src/model/subscriber.rs
+++ src/model/subscriber.rs
@@ -1,11 +1,13 @@
+use serde::{Deserialize, Serialize};
+
 use crate::model::Email;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct NewSubscriber {
     pub email: Email,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Subscriber {
     pub id: i64,
     pub email: Email,
