239764e57694cb060c92375bc6363bf38494d40c
Derrick Plotsky <derrickplotsky@gmail.com>
2023-01-12 22:49:56 -0800

    Add redirect settings and use
    This adds the redirect settings to our app data, and uses it inside of
    our handler. Also adds a test to ensure that the new functionality is
    in place, and the old functionality still works.

--- src/data.rs
+++ src/data.rs
@@ -1,9 +1,10 @@
 use sqlx::{Pool, Postgres};
 
-use crate::config::AdminSettings;
+use crate::config::{AdminSettings, SubscribedSettings};
 
 #[derive(Clone, Debug)]
 pub struct ApplicationData {
     pub admin: AdminSettings,
     pub pool: Pool<Postgres>,
+    pub subscribed: SubscribedSettings,
 }
